8e419780fd48cff682d918d990c2f9f7fe95bbe9
TanObrigan <67171508+TanObrigan@users.noreply.github.com>
2020-06-20 00:55:53 +0300

    Add files via upload

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,19 @@
 #include "revert_string.h"
+#include <string.h>
+void swap_revert_string(char *left, char *right)
+{
+char tmp = *left;
+*left = *right;
+*right = tmp;
+}
 
 void RevertString(char *str)
 {
-	// your code here
+int b=strlen(str);
+int a= b/2;
+int i;
+for (i = 0; i < a; i++)
+{
+swap_revert_string(&str[i], &str[b - i - 1]);
 }
-
+}
